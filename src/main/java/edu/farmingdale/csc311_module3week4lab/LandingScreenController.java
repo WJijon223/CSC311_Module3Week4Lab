793066b43296658c10c911d896b64aefd46dfcad
WJijon223 <williamjijon223@gmail.com>
2025-02-20 13:29:53 -0500

    added the landing screen files and moveToNewScreen functionality

---
+++ src/main/java/edu/farmingdale/csc311_module3week4lab/LandingScreenController.java
@@ -0,0 +1,4 @@
+package edu.farmingdale.csc311_module3week4lab;
+
+public class LandingScreenController {
+}
--- src/main/java/edu/farmingdale/csc311_module3week4lab/SplashScreenController.java
+++ src/main/java/edu/farmingdale/csc311_module3week4lab/SplashScreenController.java
@@ -1,14 +1,25 @@
 package edu.farmingdale.csc311_module3week4lab;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
 
 public class SplashScreenController {
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void moveToNewScreen(MouseEvent event) throws IOException {
+        System.out.println("Welcome to the landing screen");
+        Scene scene = welcomeText.getScene();
+        FXMLLoader landingScnRoot = new FXMLLoader(HelloApplication.class.getResource("landing_screen.fxml"));
+        scene.setRoot(landingScnRoot.load());
     }
+
 }
